46153688ece7080e1344dfd658d1387ba30c782d
Yue Su <su2047@gmail.com>
2021-01-04 02:22:05 -0400

    Update images.js

--- routes/images.js
+++ routes/images.js
@@ -3,4 +3,32 @@ const router = require("express").Router()
 
 const { image, user } = models
 
+router.get("/", (req, res) => {
+  image
+    .findAll({
+      where: {
+        private: false,
+      },
+      include: [user],
+      order: [["createdAt", "DESC"]],
+    })
+    .then((images) => {
+      res.status(200).json(images)
+    })
+    .catch((error) => {
+      res.status(500).json({ Error: error })
+    })
+})
+
+router.post("/", (req, res) => {
+  image
+    .bulkCreate(req.body)
+    .then((images) => {
+      res.status(200).json(images)
+    })
+    .catch((error) => {
+      res.status(500).json({ Error: error })
+    })
+})
+
 module.exports = router
