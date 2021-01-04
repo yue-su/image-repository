cbe37a67eddde63d126657e90acfeb9aed22c5cd
Yue Su <su2047@gmail.com>
2021-01-04 02:42:28 -0400

    Update images.js

--- routes/images.js
+++ routes/images.js
@@ -50,4 +50,22 @@ router.post("/", (req, res) => {
     })
 })
 
+router.delete("/", (req, res) => {
+  image
+    .destroy({
+      where: {
+        //here the req.body.id is an array of ids
+        //such as [1, 2, 3]
+        id: req.body.id,
+      },
+    })
+    //the returned value could be 0 if the items are not found
+    .then((numberOfDeleted) => {
+      res.status(200).json(numberOfDeleted)
+    })
+    .catch((error) => {
+      res.status(500).json({ Error: error })
+    })
+})
+
 module.exports = router
